3e8cbecf89f645a2f142ba8e0b8fdbecf407ec6f
joaoBrRj91 <joao1991algoritmo@gmail.com>
2023-09-01 17:13:32 -0300

    Fornecedor CRUD

--- AppCoreBasicaMVC/Models/Fornecedor.cs
+++ AppCoreBasicaMVC/Models/Fornecedor.cs
@@ -16,7 +16,7 @@ namespace AppCoreBasicaMVC.Models
             ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 11)]
         public string Documento { get; set; }
         public TipoFornecedor TipoFornedor { get; set; }
-        public Endereco Endereo { get; set; }
+        public Endereco Endereco { get; set; }
 
         [DisplayName("Ativo?")]
         public bool Ativo { get; set; }
